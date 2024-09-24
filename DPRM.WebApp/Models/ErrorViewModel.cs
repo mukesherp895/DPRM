4d4eb89b93e2c5ab169f7ffe4b7d46a673d84f37
Mukesh Rauniyar <mukesherp895@gmail.com>
2024-09-24 22:33:00 +0545

    working

---
+++ DPRM.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DPRM.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
